2d026b810ab34f2c5c1c4514dd79eb1637b71d6a
Moneesh <78355845+moneeshkumar95@users.noreply.github.com>
2021-08-10 20:25:32 +0530

    add

---
+++ Day_1.py
@@ -0,0 +1,14 @@
+#100 days of python coding challenge
+
+#Day 1 : Adding two numbers
+num_1 = input("Enter the first number: ")
+num_2 = input("Enter the second number: ")
+
+sum_1 = int(num_1)+int(num_2)
+print("\nThe Sum of %s + %s is %s " %(num_1, num_2, sum_1))
+
+sum_2 = float(num_1)+float(num_2)
+print(f"\nThe Sum of {num_1} + {num_2} is {sum_2}")
+
+
+
